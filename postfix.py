658ca613041cf0b6262ddb25130c79c74784b7a7
Gejza <gejza@rorschach>
2020-12-17 21:33:03 +0100

    opravy a zmeny

--- postfix.py
+++ postfix.py
@@ -1,30 +1,30 @@
 def eval_expr(vyraz,d={}):
-	s = []
-	vyraz=vyraz.split()
-	n=len(vyraz)    
+    s = []
+    vyraz=vyraz.split()
+    n=len(vyraz)    
     
-	for prvok in n:
-		if vyraz[prvok] in d:
-			s.append(int(d[vyraz[prvok]]))
-    	if vyraz[prvok].isdigit():
-    		s.append(int(vyraz[prvok])) 
-    		s.append(int(d[vyraz[prvok]]))
-    	if vyraz[prvok].isdigit():
-    		s.append(int(vyraz[prvok]))    
-		elif vyraz[prvok]=='+':
-        	p1=s.pop()
-        	p2=s.pop()
-        	s.append(int(p1)+int(p2))
-		elif vyraz[prvok]=='-':
-        	p1=s.pop()
-        	p2=s.pop()
-        	s.append(int(p1)-int(p2))	
+    for prvok in range(n):
+        if vyraz[prvok] in d:
+            s.append(int(d[vyraz[prvok]]))
+        if vyraz[prvok].isdigit():
+            s.append(int(vyraz[prvok])) 
+            s.append(int(d[vyraz[prvok]]))
+        if vyraz[prvok].isdigit():
+            s.append(int(vyraz[prvok]))    
+        elif vyraz[prvok]=='+':
+            p1=s.pop()
+            p2=s.pop()
+            s.append(int(p1)+int(p2))
+        elif vyraz[prvok]=='-':
+            p1=s.pop()
+            p2=s.pop()
+            s.append(int(p1)-int(p2))   
         elif vyraz[prvok]=='*':
-        	p1=s.pop()
-        	p2=s.pop()
-        	s.append(int(p1)*int(p2))
-		elif vyraz[prvok]=='/':
-        	p1=s.pop()
-        	p2=s.pop()
-        	s.append(int(p1)/int(p2))
-	return s.pop()
+            p1=s.pop()
+            p2=s.pop()
+            s.append(int(p1)*int(p2))
+        elif vyraz[prvok]=='/':
+            p1=s.pop()
+            p2=s.pop()
+            s.append(int(p1)/int(p2))
+    return s.pop()
